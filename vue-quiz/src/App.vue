d121797f6f53e10cb15217fd21942b34a375b3f7
Anas Firly <anasfirly20@gmail.com>
2023-08-30 16:30:56 +0400

    adding animation

--- vue-quiz/src/App.vue
+++ vue-quiz/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="ctr">
-    <Questions
+    <transition name="fade" mode="out-in">
+      <Questions
       v-if="questionsAnswered < questions.length"
-      :questions="questions"
-      :questionsAnswered="questionsAnswered"
-      @question-answered="questionAnswered"
-    />
-    <Result 
-    v-else 
-    :results="results"
-    :totalCorrect="totalCorrect"
-    />
+        :questions="questions"
+        :questionsAnswered="questionsAnswered"
+        @question-answered="questionAnswered"
+      />
+      <Result 
+      v-else 
+      :results="results"
+      :totalCorrect="totalCorrect"
+      />
+    </transition>
     <button 
     type="button" 
     class="reset-btn" 
